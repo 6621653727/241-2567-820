8fec6d79835dbdbdd1e738d4dd9b01b98a17ce1d
Nattachot Lukkanathiti <nattachot.l@ku.th>
2025-02-17 17:26:38 +0700

    ส่งงาน17/02/2568

--- server/index.js
+++ server/index.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 8800;
+const port = 8000;
+const mysql = require('mysql2/promise');
 app.use(bodyParser.json());
 let users = []
-let counter = 1
-app.get('/users', (req, res) => {
+let conn = null
+const initMySQL = async () => {
+  conn = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    port: 8830
+  })
+}
+app.get('/testdb-new', async (req, res) => {
+  try {
+    const result = await conn.query('SELECT * FROM users')
+    res.json(result[0])
+  }catch (error) {
+    console.log('Error fetching users: ', error.message)
+    res.status(500).json({error: 'Error fetching users'})
+  }
+})
+
+
+app.get('/users', async(req, res) => {
+  const result = await conn.query('SELECT * FROM users')
   res.json(users);
+})
+app.post('/users', async(req, res) => {
+  let user = req.body;
+  const result = await conn.query('INSERT INTO users ?',user)
+  console.log('Result: ', result)
+  res.json({
+    message: 'Data has been added',
+    data: result[0]
+  });
 })
 app.post('/user', (req, res) => {
   let user = req.body;
@@ -45,6 +75,7 @@ app.delete('/user/:id', (req, res) => {
     indexDeleted: selectedIndex
   });
 })
-app.listen(port, (req, res) => {
+app.listen(port, async(req, res) => {
+  await initMySQL()
   console.log('Server is running on port' + port);
 });
